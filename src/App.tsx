2bb4c960c824f2afbb9c628f972e33bccceee91a
Trelcray <thaliszambarda@hotmail.com>
2022-10-14 15:16:27 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/global.css'
+
+export function App() {
+
+  return (
+    <div>
+     <h1 className='font-bold text-2xl text-gray-100:'>hellow world</h1>
+    </div>
+  )
+}
---
+++ src/components/TextInput.tsx
@@ -0,0 +1,55 @@
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+import { InputHTMLAttributes, ReactNode } from 'react';
+
+
+export interface TextInputRootProps {
+    children: ReactNode;
+}
+
+TextInputRoot.displayName = 'TextInput.Root';
+
+function TextInputRoot(props: TextInputRootProps) {
+    return (
+        <div className='flex items-center h-12 gap-3 py-4 px-3 rounded bg-gray-800 w-full focus-within:ring-2 ring-cyan-300'>
+            {props.children}
+        </div>
+
+    )
+}
+
+export interface TextInputIconProps {
+    children: ReactNode;
+}
+
+function TextInputIcon(props: TextInputIconProps) {
+    return (
+        <Slot className='w-6 h-6 text-gray-400'>
+            {props.children}
+        </Slot>
+    )
+}
+
+TextInputIcon.displayName = 'TextInput.Icon';
+
+export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement> { }
+
+function TextInputInput(props: TextInputInputProps) {
+
+    return (
+
+
+        <input className='bg-transparent flex-1 text-gray-100 text-xs outline-none placeholder:text-gray-400'
+            {...props}
+        />
+
+    )
+}
+
+TextInputInput.displayName = 'TextInput.Input';
+
+export const TextInput = {
+    Root: TextInputRoot,
+    Input: TextInputInput,
+    Icon: TextInputIcon
+}
