007b3720e1cd5ce178682d189e9db37349f3f203
Trelcray <thaliszambarda@hotmail.com>
2022-10-14 15:24:13 -0300

    Config to deploy

---
+++ storybook-static/assets/preview.57c95336.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.e9f58b9b.js";import"./iframe.645ad52c.js";import"./es.map.constructor.41291982.js";import"./es.number.to-fixed.38036be0.js";import"./index.c434eae4.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.57c95336.js.map
